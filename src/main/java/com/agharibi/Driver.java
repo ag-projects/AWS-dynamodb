c4ef6ef3b0bdc84950028cf7cb5389a68404c80f
Armen G <gharibi.armen@yahoo.com>
2018-09-01 16:05:34 -0700

    Added new logic to save Item Objects to AWS DynamoDB

--- src/main/java/com/agharibi/Driver.java
+++ src/main/java/com/agharibi/Driver.java
@@ -6,7 +6,10 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class Driver {
 
@@ -15,7 +18,47 @@ public class Driver {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withRegion(Regions.US_EAST_1)
                 .build();
-        lowLevelDemo(client);
+        // lowLevelDemo(client);
+        highLevelDemo(client);
+    }
+
+    private static void print(List<Item> all) {
+        System.out.println(all.stream()
+                .map(Item::toString)
+                .collect(Collectors.joining("\n")));
+    }
+
+    private static Item newItem(String name, String description) {
+        Item item = new Item();
+        
+        item.setName(name);
+        item.setDescription(description);
+
+        return item;
+    }
+
+    private static void pause() {
+        System.out.println("PAUSE");
+        try {
+            System.in.read();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void highLevelDemo(AmazonDynamoDB client) {
+        ItemDao itemDao = new ItemDao(client);
+
+        Item item1 = itemDao.put(newItem("Mac Book Pro", "The very Best"));
+        Item item2 = itemDao.put(newItem("Apple TV", "Black & White"));
+        Item item3 = itemDao.put(newItem("Mac Desktop", "As good as always"));
+
+        print(itemDao.getAll());
+        pause();
+
+        itemDao.delete(item2.getId());
+        print(itemDao.getAll());
+
     }
 
     private static void lowLevelDemo(AmazonDynamoDB client) {
--- src/main/java/com/agharibi/dao/ItemDao.java
+++ src/main/java/com/agharibi/dao/ItemDao.java
@@ -2,45 +2,35 @@ package com.agharibi.dao;
 
 import com.agharibi.domain.Item;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class ItemDao {
 
-    private final AmazonDynamoDB dynamoDB;
+    private final DynamoDBMapper mapper;
 
     public ItemDao(AmazonDynamoDB dynamoDB) {
-        this.dynamoDB = dynamoDB;
+        this.mapper = new DynamoDBMapper(dynamoDB);
     }
 
-    public void put(Item item) {
-        Map<String, AttributeValue> itemMap = new HashMap<>();
-
-        itemMap.put("id",
-                new AttributeValue().withS(item.getId()));
-
-        if (item.getName() != null) {
-            itemMap.put("name",
-                    new AttributeValue().withS(item.getName()));
-        }
-
-        if (item.getDescription() != null) {
-            itemMap.put("description",
-                    new AttributeValue().withS(item.getDescription()));
-        }
-
-        itemMap.put("totalRating",
-                new AttributeValue().withN(Integer.toString(item.getTotalRating())));
-
-        itemMap.put("totalComments",
-                new AttributeValue().withN(Integer.toString(item.getTotalComments())));
-
-        PutItemRequest putItemRequest = new PutItemRequest("Items", itemMap);
-        dynamoDB.putItem(putItemRequest);
+    public Item put(Item item) {
+        mapper.save(item);
+        return item;
     }
 
+    public Item get(String id) {
+        return mapper.load(Item.class, id);
+    }
 
+    public void delete(String id) {
+        Item item = new Item();
+        item.setId(id);
+        mapper.delete(item);
+    }
+
+    public List<Item> getAll() {
+        return mapper.scan(Item.class, new DynamoDBScanExpression());
+    }
 }
